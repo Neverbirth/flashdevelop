881be4e32da3a05fc510087fe4a2c31fb87d861e
Neverbirth <neverbirth@gmail.com>
2016-03-01 23:11:24 +0100

    Test

--- CI/tests.ps1
+++ CI/tests.ps1
@@ -1,4 +1,4 @@
-# Deploys the built binaries to the FD.org server
+# Runs the unit tests, and uploads them to the CI server
 
 Param (
     $variables = @{},   
@@ -12,24 +12,30 @@ Param (
     $projectBuildNumber
 )
 
-$login = $variables["SecureLogin"]
-$pass = $variables["SecurePass"]
-
-If (Test-Path "nunit-console-x86.exe"){
-	$testFiles = Get-ChildItem FlashDevelop\Bin\Debug -filter "*.Tests.dll")
-	IF ($testFiles.Count == 0)
+If (Test-Path "nunit-console-x86.exe")
+{
+	$testFiles = [System.IO.Directory]::GetFiles("..\FlashDevelop\Bin\Debug", "*.Tests.dll")
+	IF ($testFiles.Count -eq 0)
     {
     	Write-Output "No test assemblies found"
     }
     ELSE
     {
+        $testErrors = $false
+        #Should we break on first error? what if we want to set categories and prioritize them? think later if we want this
         foreach($testFile in $testFiles)
         {
-            nunit-console-x86.exe "$($testFile.FullName)"
-            $wc = New-Object 'System.Net.WebClient'
-            $wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestResult.xml))
+            nunit-console-x86.exe "$testFile"
+            $testErrors = $testErrors -or $LASTEXITCODE -ne 0
+            if ((Test-Path env:\APPVEYOR_JOB_ID) -And (Test-Path TestResult.xml))
+            {
+                $wc = New-Object 'System.Net.WebClient'
+                $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestResult.xml))
+            }
         }
-    }
+
+        if ($testErrors -eq $true)
+        {            exit 1        }    }
 }
 ELSE
 {
